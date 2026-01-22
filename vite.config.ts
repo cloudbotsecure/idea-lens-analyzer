501c9c09592875f617860ecce4707722e8fdc847
faceitclouds-eng <faceitclouds@gmail.com>
2026-01-23 00:46:09 +0100

    fix base path for pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/idea-lens-analyzer/",
+
   server: {
     host: "::",
     port: 8080,
-	base: '/',
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
